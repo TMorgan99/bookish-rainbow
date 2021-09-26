d080fa959a9c51e77481b2e60b6400a8c07965e1
Tim Morgan <TMorgan99@GMail.com>
2021-09-26 15:44:09 -0700

    #10

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,24 @@ module.exports = config => {
   // Set directories to pass through to the dist folder
   config.addPassthroughCopy('./src/images/')
 
+  // --- COLLECTION ---
+  // Returns work items, sorted by display order
+  // These should really just be YAML,
+  // or even better, CSV!
+  config.addCollection('work', collection => {
+    return collection
+      .getFilteredByGlob('./src/work/*.md')
+      .sort((a, b) => (Number(a.data.displayOrder) > Number(b.data.displayOrder) ? 1 : -1));
+  });
+  // cannot I simply filter my existing collection ???
+  config.addCollection('featuredWork', collection => {
+    return collection
+      .getFilteredByGlob('./src/work/*.md')
+      .sort((a, b) => (Number(a.data.displayOrder) > Number(b.data.displayOrder) ? 1 : -1))
+      .filter(x => x.data.featured);
+  });
+
+
   return {
     // markdownTemplateEngine: 'njk',
     // dataTemplateEngine: 'njk',
