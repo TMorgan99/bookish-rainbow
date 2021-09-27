a741dad469ddd358cf389b7ac4f59cc54501cff8
Tim Morgan <TMorgan99@GMail.com>
2021-09-26 17:29:58 -0700

    #12

---
+++ src/filters/date-filter.js
@@ -0,0 +1,7 @@
+// npm install moment
+const moment = require('moment');
+
+module.exports = value => {
+  const dateObject = moment(value);
+  return `${dateObject.format('Do')} of ${dateObject.format('MMMM YYYY')}`;
+};
