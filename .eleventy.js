bfbb10f9f5675b05ffe4925ae264317f71795d36
Tim Morgan <tmorgan99@gmail.com>
2021-09-29 17:10:59 -0700

    format cleaning

--- .eleventy.js
+++ .eleventy.js
@@ -1,46 +1,43 @@
 // Main Config!
 
 // require Filters
-const dateFilter = require('./src/filters/date-filter.js');
-const w3DateFilter = require('./src/filters/w3-date-filter.js');
+const dateFilter = require("./src/filters/date-filter.js");
+const w3DateFilter = require("./src/filters/w3-date-filter.js");
 
-const rssPlugin = require('@11ty/eleventy-plugin-rss');
+const rssPlugin = require("@11ty/eleventy-plugin-rss");
 
 // Transforms
-const htmlMinTransform = require('./src/transforms/html-min-transform.js');
+const htmlMinTransform = require("./src/transforms/html-min-transform.js");
 
 // Create a helpful production flag
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === "production";
 
-module.exports = config => {
-
+module.exports = (config) => {
   // Add filters
-  config.addFilter('dateFilter', dateFilter);
-  config.addFilter('w3DateFilter', w3DateFilter);
+  config.addFilter("dateFilter", dateFilter);
+  config.addFilter("w3DateFilter", w3DateFilter);
 
   // Plugins
   config.addPlugin(rssPlugin);
 
   // Set directories to pass through to the dist folder
   // config.addPassthroughCopy('./src/images/')
-// Only minify HTML if we are in production because it slows builds _right_ down
-if (isProduction) {
-  config.addTransform('htmlmin', htmlMinTransform);
-}
+  // Only minify HTML if we are in production because it slows builds _right_ down
+  if (isProduction) {
+    config.addTransform("htmlmin", htmlMinTransform);
+  }
 
   // --- COLLECTION ---
   // - Blog
   // Returns a collection of blog posts in reverse date order
-  config.addCollection('blog', collection => {
-    return [
-      ...collection.getFilteredByGlob('./src/posts/*.md')
-    ].reverse();
+  config.addCollection("blog", (collection) => {
+    return [...collection.getFilteredByGlob("./src/posts/*.md")].reverse();
   });
 
   // - People
   // Returns a list of people ordered by filename
-  config.addCollection('people', collection => {
-    return collection.getFilteredByGlob('./src/people/*.md').sort((a, b) => {
+  config.addCollection("people", (collection) => {
+    return collection.getFilteredByGlob("./src/people/*.md").sort((a, b) => {
       return Number(a.fileSlug) > Number(b.fileSlug) ? 1 : -1;
     });
   });
@@ -49,26 +46,28 @@ if (isProduction) {
   // Returns work items, sorted by display order
   // These should really just be YAML,
   // or even better, CSV!
-  config.addCollection('work', collection => {
+  config.addCollection("work", (collection) => {
     return collection
-      .getFilteredByGlob('./src/work/*.md')
-      .sort((a, b) => (Number(a.data.displayOrder) > Number(b.data.displayOrder) ? 1 : -1));
+      .getFilteredByGlob("./src/work/*.md")
+      .sort((a, b) =>
+        Number(a.data.displayOrder) > Number(b.data.displayOrder) ? 1 : -1
+      );
   });
   // cannot I simply filter my existing collection ???
-  config.addCollection('featuredWork', collection => {
+  config.addCollection("featuredWork", (collection) => {
     return collection
-      .getFilteredByGlob('./src/work/*.md')
-      .sort((a, b) => (Number(a.data.displayOrder) > Number(b.data.displayOrder) ? 1 : -1))
-      .filter(x => x.data.featured);
+      .getFilteredByGlob("./src/work/*.md")
+      .sort((a, b) =>
+        Number(a.data.displayOrder) > Number(b.data.displayOrder) ? 1 : -1
+      )
+      .filter((x) => x.data.featured);
   });
-
 
   return {
     // markdownTemplateEngine: 'njk',
     // dataTemplateEngine: 'njk',
-    htmlTemplateEngine: 'njk',
+    htmlTemplateEngine: "njk",
 
-    dir: { input: 'src', output: 'dist' },
-
-  }
-}
+    dir: { input: "src", output: "dist" },
+  };
+};
--- NOTES.md
+++ NOTES.md
@@ -1,21 +1,18 @@
 NOTES.md
 
-
-There was a problem!  My formatter was messing up my .html <script> tags that included {% nunjucks %}
+There was a problem! My formatter was messing up my .html <script> tags that included {% nunjucks %}
 
 As suggested int the eseom nunjucks extension, I installed .
 
 "files.associations": {
-  "*.html": "njk"
+"\*.html": "njk"
 },
 
 after adding
-    "html.format.endWithNewline": true,
-    "html.format.indentInnerHtml": true,
-    "html.format.templating": true,
+"html.format.endWithNewline": true,
+"html.format.indentInnerHtml": true,
+"html.format.templating": true,
 
 as suggested [here](https://piccalil.li/course/learn-eleventy-from-scratch/lesson/15/)
 
-
 gulp-imagemin ^7 is ok as a CJS, ^8 is a module !
-
